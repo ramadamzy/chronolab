2f71e6478f2998a5756b05dc621a51edd5109dd4
Ramzy <ramzy.hussein.bee@gmail.com>
2026-06-23 15:23:16 +0200

    Start project met intro scherm

---
+++ main.py
@@ -0,0 +1,7 @@
+print("=== CHRONOLAB ===")
+print("Welkom in het Tijdreis Laboratorium!")
+
+naam = input("Wat is je naam? ")
+
+print(f"Welkom {naam}!")
+print("Jouw missie: red de toekomst door tijd te reizen.")
